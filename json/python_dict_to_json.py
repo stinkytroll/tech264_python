ebd768beae13469a674e4d3ccac4a4a88f18c29e
stinkytroll <61510983+stinkytroll@users.noreply.github.com>
2024-09-26 17:21:37 +0100

    Added json research

---
+++ json/python_dict_to_json.py
@@ -0,0 +1,30 @@
+import json
+from pprint import pprint  # Import pretty print
+
+# create the dictionary
+servers_dict = {
+    "server1": {
+        "hostname": "web-server-1",
+        "ip_address": "192.168.1.1",
+        "role": "web",
+        "status": "active"
+    },
+    "server2": {
+        "hostname": "db-server-1",
+        "ip_address": "192.168.1.2",
+        "role": "database",
+        "status": "maintenance"
+    }
+}
+
+json_formatting = json.dumps(servers_dict)  # Serialises a Python object into a JSON-formatted string.
+pprint(json_formatting)
+
+with open('servers.json', 'w') as json_file:
+    json.dump(servers_dict, json_file, indent=4)
+    print("Data saved to servers.json")
+
+# with statement creates context for the execution of code. open() is then used to open a selected file for
+# interaction. We use 'w' to enable write mode, allowing us to open the file for writing. We then convert and write
+# the dictionary to JSON format as a json file, as well as a parameter for indenting the lines 4 times for readability.
+
--- modules_libraries_packages/simple_calc.py
+++ modules_libraries_packages/simple_calc.py
@@ -1,27 +1,37 @@
 from math_operations import *
 
 # Mini-calculator
+user_input = input("Enter a number to access a particular operation (1 = Addition, 2 = Subtraction, "
+                   "3 = Multiplication, 4 = Division):")
 
 # Add
-first_num = int(input("Enter the first number: "))
-second_num = int(input("Enter the first number: "))
-result = add(first_num, second_num)
-print(f"{first_num} + {second_num} = {result}")
+if user_input == "1":
+    print("You selected addition!")
+    first_num = int(input("Enter the first number: "))
+    second_num = int(input("Enter the first number: "))
+    result = add(first_num, second_num)
+    print(f"{first_num} + {second_num} = {result}")
 
 # Subtract
-first_num = int(input("Enter the first number: "))
-second_num = int(input("Enter the first number: "))
-result = subtract(first_num, second_num)
-print(f"{first_num} + {second_num} = {result}")
+if user_input == "2":
+    print("You selected subtraction!")
+    first_num = int(input("Enter the first number: "))
+    second_num = int(input("Enter the first number: "))
+    result = subtract(first_num, second_num)
+    print(f"{first_num} - {second_num} = {result}")
 
 # Multiply
-first_num = int(input("Enter the first number: "))
-second_num = int(input("Enter the first number: "))
-result = multiply(first_num, second_num)
-print(f"{first_num} + {second_num} = {result}")
+if user_input == "3":
+    print("You selected multiplication!")
+    first_num = int(input("Enter the first number: "))
+    second_num = int(input("Enter the first number: "))
+    result = multiply(first_num, second_num)
+    print(f"{first_num} * {second_num} = {result}")
 
 # Divide
-first_num = int(input("Enter the first number: "))
-second_num = int(input("Enter the first number: "))
-result = division(first_num, second_num)
-print(f"{first_num} + {second_num} = {result}")
+if user_input == "4":
+    print("You selected division!")
+    first_num = int(input("Enter the first number: "))
+    second_num = int(input("Enter the first number: "))
+    result = division(first_num, second_num)
+    print(f"{first_num} / {second_num} = {result}")
