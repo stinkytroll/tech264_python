e521698a7ad5a90932f9501afa49689ab1d9051a
stinkytroll <61510983+stinkytroll@users.noreply.github.com>
2024-09-29 13:33:07 +0100

    Readjusted indents

--- make_functions/dice_roll_game.py
+++ make_functions/dice_roll_game.py
@@ -1,67 +1,64 @@
 import random
 
-
-def roll_dice():
-    return random.randint(1, 6)
-
+dice = random.randint(1, 6)
 
 game_choice = input("Yo. Let's play dice. Wanna do a best of 10 or first to 30? "
                     "Say 1 for best of 10, say 2 for first to 30: ")
 while game_choice not in ["1", "2"]:
     game_choice = input("That's not a valid option, dummy. Try again: ")
 
-    if game_choice == "1":
-        name = input("So you want to roll some dice, eh? Tell me when your name when you're ready: ")
-        print(f"{name} is little weird but sure. Let's play.")
-        player_wins = 0
-        computer_wins = 0
+if game_choice == "1":
+    name = input("So you want to roll some dice, eh? Tell me when your name when you're ready: ")
+    print(f"{name} is little weird but sure. Let's play.")
+    player_wins = 0
+    computer_wins = 0
 
-        while computer_wins < 10 and player_wins < 10:
-            roll = input('Say "Roll" to roll the dice: ')
-            if roll == 'Roll':
-                player_dice = roll_dice()
-                print(f"You rolled a {player_dice}")
-                computer_dice = roll_dice()
-                print(f"I rolled a {computer_dice}")
-                if player_dice > computer_dice:
-                    player_wins += 1
-                    print(f"Hm. your {player_dice} beat my {computer_dice}. How lucky. You win. "
-                          f"The score is {player_wins} to you, {computer_wins} to me.")
+    while computer_wins < 10 and player_wins < 10:
+        roll = input('Say "Roll" to roll the dice: ')
+        if roll == 'Roll':
+            player_dice = random.randint(1, 6)
+            print(f"You rolled a {player_dice}")
+            computer_dice = random.randint(1, 6)
+            print(f"I rolled a {computer_dice}")
+            if player_dice > computer_dice:
+                player_wins += 1
+                print(f"Hm. your {player_dice} beat my {computer_dice}. How lucky. You win. "
+                      f"The score is {player_wins} to you, {computer_wins} to me.")
 
-                elif player_dice == computer_dice:
-                    print(f"Seems like we both rolled a {computer_dice}. It's a draw. No points for either of us.")
-                else:
-                    computer_wins += 1
-                    print(f"Hm. my {computer_dice} beat your {player_dice}. I win, chump. "
-                          f"The score is {player_wins} to you, {computer_wins} to me.")
-
+            elif player_dice == computer_dice:
+                print(f"Seems like we both rolled a {computer_dice}. It's a draw. No points for either of us.")
             else:
-                print('You need to say "Roll". Try again.')
+                computer_wins += 1
+                print(f"Hm. my {computer_dice} beat your {player_dice}. I win, chump. "
+                      f"The score is {player_wins} to you, {computer_wins} to me.")
 
-        if computer_wins == 10:
-            print("Looks like I win, loser. You owe me a virtual donut.")
         else:
-            print(f"Wow. You actually beat me, good job {name}.")
+            print('You need to say "Roll". Try again.')
 
-    elif game_choice == "2":
-        name = input("So you wanna play first to 30, eh? Tell me when your name when you're ready: ")
-        print(f"{name} is little weird but sure. Let's play.")
-        player_score = 0
-        computer_score = 0
+    if computer_wins == 10:
+        print("Looks like I win, loser. You owe me a virtual donut.")
+    else:
+        print(f"Wow. You actually beat me, good job {name}.")
 
-        while computer_score < 30 and player_score < 30:
-            roll = input('Say "Roll" to roll the dice: ')
-            if roll == 'Roll':
-                player_dice = roll_dice()
-                print(f"You rolled a {player_dice}")
-                computer_dice = roll_dice()
-                print(f"I rolled a {computer_dice}")
-                player_score += player_dice
-                computer_score += computer_dice
-                print(f"That brings you to {player_score} and me to {computer_score}.")
-        if player_score >= 30:
-            print("You win! I hate you!")
-        elif computer_score >= 30:
-            print("I win! You stink.")
-        else:
-            print("Wow. We actually drew. Interesting.")
+elif game_choice == "2":
+    name = input("So you wanna play first to 30, eh? Tell me when your name when you're ready: ")
+    print(f"{name} is little weird but sure. Let's play.")
+    player_score = 0
+    computer_score = 0
+
+    while computer_score < 30 and player_score < 30:
+        roll = input('Say "Roll" to roll the dice: ')
+        if roll == 'Roll':
+            player_dice = random.randint(1, 6)
+            print(f"You rolled a {player_dice}")
+            computer_dice = random.randint(1, 6)
+            print(f"I rolled a {computer_dice}")
+            player_score += player_dice
+            computer_score += computer_dice
+            print(f"That brings you to {player_score} and me to {computer_score}.")
+    if player_score >= 30:
+        print("You win! I hate you!")
+    elif computer_score >= 30:
+        print("I win! You stink.")
+    else:
+        print("Wow. We actually drew. Interesting.")
